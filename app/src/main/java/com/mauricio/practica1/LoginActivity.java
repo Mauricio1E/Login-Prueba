6182aa91f47be5ba69afc2d7a4cf88266aec9516
mauricio1E <mauricio20091900@live.com.mx>
2017-10-07 19:33:42 +0200

    Validacion del Login

--- app/src/main/java/com/mauricio/practica1/LoginActivity.java
+++ app/src/main/java/com/mauricio/practica1/LoginActivity.java
@@ -2,14 +2,33 @@ package com.mauricio.practica1;
 
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.speech.tts.Voice;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class LoginActivity extends BaseActivity {
 
-private Toolbar toolbar;
 
-    private static final String TAG = LoginActivity.class.getName() ;
+    private static final String TAG = LoginActivity.class.getName();
+
+
+    private Toolbar toolbar;
+
+    @BindView(R.id.edt_email)
+    EditText email;
+    @BindView(R.id.text_email)
+    TextInputLayout textEmail;
+    @BindView(R.id.edt_password)
+    EditText password;
+    @BindView(R.id.text_password)
+    TextInputLayout textPassword;
 
     @Override
     protected int getLayoutResId() {
@@ -22,51 +41,46 @@ private Toolbar toolbar;
         initConfigToolbar();
     }
 
-    private void initConfigToolbar(){
-        toolbar=getToolbar();
+    private void initConfigToolbar() {
+        toolbar = getToolbar();
         toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back_white_24dp));
     }
 
-    /*
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.e(TAG, "Entro al on create");
+    @OnClick(R.id.btn_entrar)
+    public void onClickLogin() {
+        if (isValidLogin()) {
+
+        }
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.e(TAG, "Entro al onStart");
+    public boolean isValidLogin() {
+        boolean valid = true;
+        textEmail.setError(null);
+        textPassword.setError(null);
+        textEmail.setErrorEnabled(false);
+        textPassword.setErrorEnabled(false);
+        if (email.getText().toString().isEmpty()) {
+            valid = false;
+            textEmail.setError(getString(R.string.login_text_error_is_empty_email));
+        }
+        if (password.getText().toString().isEmpty()) {
+            valid = false;
+            textPassword.setError(getString(R.string.login_text_error_is_empty_password));
+        }
+        if (!isEmailValid(email.getText().toString())){
+            valid=false;
+            textEmail.setError(getString(R.string.login_text_error_is_valis_email));
+        }
+        return valid;
+
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.e(TAG, "Entro al onResume");
+    private boolean isEmailValid(CharSequence charSequence){
+        if (TextUtils.isEmpty(charSequence)){
+            return false;
+        }else{
+            return Patterns.EMAIL_ADDRESS.matcher(charSequence).matches();
+        }
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.e(TAG, "entro al onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.e(TAG, "entro al onStop");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.e(TAG, "entro al onRestart");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.e(TAG, "entro al onDestroyed");
-    }*/
 }
